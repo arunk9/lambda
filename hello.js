d05bf62ebd78292b8e9288201d6610e619cc2cd8
Arun <ak97165@gmail.com>
2021-06-16 21:12:15 +0530

    testing

--- hello.js
+++ hello.js
@@ -1,3 +1,10 @@
-exports.handler = function (event, context) {
-    context.succeed('hello world');
+exports.handler = async (event) => {
+    return {
+        statusCode: 200,
+        statusDescription: "200 OK",
+        body: JSON.stringify('Hello from Lambda!!!!!!!!'),
+        headers: {
+            "Content-Type" : "text/html"
+        }
+    };
 };
